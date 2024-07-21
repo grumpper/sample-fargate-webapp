b2f7c26fb2499a28ba9b6a6e88a1e52323b617b9
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-07-21 19:18:15 +0300

    enable nat gw

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
 }
 
 # Create ALB to expose the Fargate service
